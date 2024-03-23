e26cd9bb622ed6e6839239759049df2fd7f83fee
Czar <caissar.senac@gmail.com>
2024-03-23 20:33:47 -0300

    Create bookshelf model

---
+++ resources/views/books.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="{{ asset('css/books-style.css') }}">
+    </head>
+    <body>
+        <div class="books-container mx-auto">
+            <div class="book">livro</div>
+            <div class="book">livro</div>
+            <div class="book">livro</div>
+            <div class="book">livro</div>
+            <div class="book">livro</div>
+        </div>
+    </body>
+</html>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,5 +13,6 @@
                 </div>
             </div>
         </div>
+        @include('books')
     </div>
 </x-app-layout>
