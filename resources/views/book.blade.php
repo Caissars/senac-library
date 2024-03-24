5ac9a508c23380d43f7647cf62d284c4e0d8e4b2
Czar <caissar.senac@gmail.com>
2024-03-23 21:43:55 -0300

    Create info of book page style

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+<!-- Book page info-->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="{{ asset('css/book.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <img src="https://m.media-amazon.com/images/I/819js3EQwbL._AC_UF1000,1000_QL80_.jpg" alt="">
+            <div class="info">
+                <h1>titulo</h1>
+            </div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Simulate book list
+
+Route::get('/book', function () {
+    return view('book');
+});
+
 require __DIR__.'/auth.php';
