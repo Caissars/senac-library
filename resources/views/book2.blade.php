673f66343747045c70b977d905c97146262ff0cb
Czar <caissar.senac@gmail.com>
2024-03-24 11:14:31 -0300

    create books page

---
+++ resources/views/book2.blade.php
@@ -0,0 +1,18 @@
+<!-- Book page info-->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="{{ asset('css/book.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <img src="https://m.media-amazon.com/images/I/819js3EQwbL._AC_UF1000,1000_QL80_.jpg" alt="">
+            <div class="info">
+                <h1>titulo</h1>
+                <h2>Autor</h2>
+                <h3>Categorias</h3>
+            </div>
+        </div>
+    </body>
+</html>
