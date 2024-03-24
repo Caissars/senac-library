b0dfe0850f6bd9241b3f2cc4d22a34aa2d202270
Czar <caissar.senac@gmail.com>
2024-03-24 11:38:20 -0300

    Alter book2 info

--- resources/views/book2.blade.php
+++ resources/views/book2.blade.php
@@ -9,9 +9,9 @@
         <div class="container">
             <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F-M9R-qir2jd0%2FUfxcUF2kzFI%2FAAAAAAAAAFk%2F3LIJLgjnPTU%2Fs1600%2Fdivinacomedia.jpg&f=1&nofb=1&ipt=794fc553408ad19b167f79af7632a9489e52974b18df0ff6efa1c2f0be0445f3&ipo=images" alt="">
             <div class="info">
-                <h1>titulo</h1>
-                <h2>Autor</h2>
-                <h3>Categorias</h3>
+                <h1>A divina comédia</h1>
+                <h2>Dante Alighieri</h2>
+                <h3>Comédia</h3>
             </div>
         </div>
     </body>
